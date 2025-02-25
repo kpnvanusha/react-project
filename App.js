eceabff93723ea11547c2a8e990623d2a5e6452f
pnvanusha.k <pnvanusha.k@salesforce.com>
2025-02-25 16:19:27 +0530

    intial commit

---
+++ App.js
@@ -0,0 +1,3 @@
+const heading = React.createElement('h1', {'id': 'heading','className':'main-heading'}, 'Hello React using library from external file');
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
